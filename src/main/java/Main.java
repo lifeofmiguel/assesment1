d3f539ee664a3a9e9a293c10674667202218ae7a
Miguel Morales <miguelmorales@Miguels-MacBook-Pro.local>
2022-09-13 14:22:36 -0400

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(char(alph));
+    }
+
+    char[] alph = {'a', 'b', 'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+
+}
---
+++ src/main/java/Q3.java
@@ -0,0 +1,7 @@
+public class Q3 {
+    public static void main(String[] args) {
+
+    }
+    int[] numbers = {9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,};
+
+}
