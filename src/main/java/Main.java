21e1554b576dffdd0289e853affeea68c5b360cd
Miguel Morales <miguelmorales@Miguels-MacBook-Pro.local>
2022-09-14 19:38:30 -0400

    fixed alphabet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,29 @@
 class Main {
     public static void main(String[] args) {
-        char alph;
+//        char alph;
+//        for(alph = 'A'; alph <= 'Z'; ++alph)
+//            System.out.print(alph);
+//        for(int number=1; number<=20; number++){
+//            int i = 9;
+//           if ((i <= 9));
+//            System.out.println(number);
+//        }
 
-        for(alph = 'A'; alph <= 'Z'; ++alph)
-            System.out.print(alph);
+        char[] alph = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
+        System.out.println(alph[alph.length - 2]);
 
-        for(int number=9; number<=20; number++){
-            int i;
-           // if ((i <= 9));
-            System.out.println(number);
+        alph[0] = '$';
+
+        System.out.println(alph[alph.length - 2]);
+
+        for (int indexPostion = 0; indexPostion < alph.length; indexPostion++){
+            if (alph[indexPostion] != 'y'){
+                System.out.println(alph[indexPostion]);
+            }
         }
+
+
     }
 
 }
