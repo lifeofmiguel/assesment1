a0ba1f7a0815e55120662715377dd191e8079589
Miguel Morales <miguelmorales@Miguels-MacBook-Pro.local>
2022-09-14 20:51:33 -0400

    finished q3

---
+++ src/main/java/question3/Main.java
@@ -0,0 +1,15 @@
+package question3;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 20;
+
+        while (i >= 0){
+            if(i >= 10){
+                System.out.println(i);
+            }
+
+            i--;
+        }
+    }
+}
